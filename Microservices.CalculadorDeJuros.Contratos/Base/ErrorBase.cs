01924935c709dc33b07df43727d583ef60c9410a
Matheus Sampaio <sampaio.brenner@gmail.com>
2020-03-08 14:20:54 -0300

    Add project files.

---
+++ Microservices.CalculadorDeJuros.Contratos/Base/ErrorBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Microservices.CalculadorDeJuros.Contratos.Base
+{
+    public abstract class ErrorBase
+    {
+        protected ErrorBase() => Errors = new List<string>();
+
+        public ICollection<string> Errors { get; protected set; }
+
+        public bool IsValid => Errors.Count == 0;
+
+        public void AddError(string erro)
+        {
+            if (!Errors.Contains(erro))
+                Errors.Add(erro);
+        }
+
+        public void AddError(ICollection<string> erros) => erros.ToList().ForEach(AddError);
+    }
+}
